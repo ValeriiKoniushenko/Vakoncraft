cd984b6af85054a520bdf68a43828b3555dcca6a
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2023-01-07 20:27:38 +0200

    Added Character

---
+++ Source/Vakoncraft/Public/Core/Character/MainCharacter.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MainCharacter.generated.h"
+
+UCLASS()
+class VAKONCRAFT_API AMainCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AMainCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
