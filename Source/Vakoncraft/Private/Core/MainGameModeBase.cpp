1936dfffa2365fbabd77ee68048cda5d037307dc
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2023-01-07 20:40:28 +0200

    Setup MainGameModeBase

--- Source/Vakoncraft/Private/Core/MainGameModeBase.cpp
+++ Source/Vakoncraft/Private/Core/MainGameModeBase.cpp
@@ -2,7 +2,17 @@
 
 
 #include "Core/MainGameModeBase.h"
+#include "Core/Character/MainCharacter.h"
+#include "Core/Character/MainPlayerController.h"
+#include "Core/HUD/MainHUD.h"
+#include "Core/MainPlayerState.h"
+#include "Core/MainGameStateBase.h"
 
 AMainGameModeBase::AMainGameModeBase()
 {
+	GameStateClass = AMainGameStateBase::StaticClass();
+	HUDClass = AMainHUD::StaticClass();
+	PlayerControllerClass = AMainPlayerController::StaticClass();
+	PlayerStateClass = AMainPlayerState::StaticClass();
+	DefaultPawnClass = AMainCharacter::StaticClass();
 }
